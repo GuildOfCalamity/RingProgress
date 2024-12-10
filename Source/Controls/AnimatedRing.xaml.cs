d1db11596ca8c9a22d7df6376e22145124d342b0
GuildOfCalamity <81915293+GuildOfCalamity@users.noreply.github.com>
2024-12-10 16:45:21 -0500

    initial commit

---
+++ Source/Controls/AnimatedRing.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System.Diagnostics;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace RingProgressDemo.Controls;
+
+public partial class AnimatedRing : UserControl
+{
+    bool _hasAppliedTemplate = false;
+
+    #region [Dependency Properties]
+    /// <summary>
+    ///   Define our arc height property
+    /// </summary>
+    public static readonly DependencyProperty HeightProperty = DependencyProperty.Register(
+        nameof(Height),
+        typeof(double),
+        typeof(AnimatedRing),
+        new PropertyMetadata(50d, OnHeightChanged));
+    public double Height
+    {
+        get { return (double)this.GetValue(HeightProperty); }
+        set { this.SetValue(HeightProperty, value); }
+    }
+    static void OnHeightChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        var cp = d as AnimatedRing;
+        if (cp != null && e.NewValue != null)
+        {
+            Debug.WriteLine($"[INFO] Height changed to {e.NewValue}");
+        }
+    }
+
+    /// <summary>
+    ///   Define our arc width property
+    /// </summary>
+    public static readonly DependencyProperty WidthProperty = DependencyProperty.Register(
+        nameof(Width),
+        typeof(double),
+        typeof(AnimatedRing),
+        new PropertyMetadata(50d, OnWidthChanged));
+    public double Width
+    {
+        get { return (double)this.GetValue(WidthProperty); }
+        set { this.SetValue(WidthProperty, value); }
+    }
+    static void OnWidthChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        var cp = d as AnimatedRing;
+        if (cp != null && e.NewValue != null)
+        {
+            Debug.WriteLine($"[INFO] Width changed to {e.NewValue}");
+        }
+    }
+    #endregion
+
+    public AnimatedRing()
+    {
+        InitializeComponent();
+    }
+
+    public override void OnApplyTemplate()
+    {
+        base.OnApplyTemplate();
+        _hasAppliedTemplate = true;
+        //leadingArc.GradientStops.Clear();
+        //leadingArc.GradientStops.Add(new GradientStop(Color.FromArgb(255, 204, 255, 0), 0.2));
+        //leadingArc.GradientStops.Add(new GradientStop(Color.FromArgb(51, 255, 204, 0), 1.0));
+        Debug.WriteLine($"[INFO] {nameof(AnimatedRing)} template has been applied.");
+    }
+
+    protected override Size MeasureOverride(Size availableSize)
+    {
+        Debug.WriteLine($"[INFO] {nameof(AnimatedRing)} is measured to be {availableSize}");
+        return base.MeasureOverride(availableSize);
+    }
+}
---
+++ Source/Controls/TrackRing.cs
@@ -0,0 +1,135 @@
+﻿using System.Windows;
+using System.ComponentModel;
+using System.Windows.Controls;
+
+namespace RingProgressDemo.Controls;
+
+/// <summary>
+///    NOTE: This must be accompanied by the &lt;Style TargetType="{x:Type ctrl:ProgressRing}"&gt; in your App.xaml resource dictionary.
+/// </summary>
+[TemplateVisualState(GroupName = VisualStates.GroupActive, Name = VisualStates.StateActive)]
+[TemplateVisualState(GroupName = VisualStates.GroupActive, Name = VisualStates.StateInactive)]
+public partial class TrackRing : Control
+{
+    bool _hasAppliedTemplate;
+
+    #region [Dependency Properties]
+    public static readonly DependencyProperty IsActiveProperty = DependencyProperty.Register(
+        nameof(IsActive),
+        typeof(bool),
+        typeof(TrackRing),
+        new PropertyMetadata(false, IsActiveChanged));
+    public bool IsActive
+    {
+        get => (bool)GetValue(IsActiveProperty);
+        set => SetValue(IsActiveProperty, value);
+    }
+
+    public static readonly DependencyProperty TemplateSettingsProperty = DependencyProperty.Register(
+        nameof(TemplateSettings),
+        typeof(TemplateSettingValues),
+        typeof(TrackRing),
+        new PropertyMetadata(null));
+    public TemplateSettingValues TemplateSettings
+    {
+        get => (TemplateSettingValues)GetValue(TemplateSettingsProperty);
+        set => SetValue(TemplateSettingsProperty, value);
+    }
+    #endregion
+
+    public TrackRing()
+    {
+        DefaultStyleKey = typeof(TrackRing);
+        TemplateSettings = new TemplateSettingValues(60);
+    }
+
+    public override void OnApplyTemplate()
+    {
+        base.OnApplyTemplate();
+        _hasAppliedTemplate = true;
+        UpdateState(IsActive);
+    }
+
+    protected override Size MeasureOverride(Size availableSize)
+    {
+        var width = 40d;
+        var height = 40d;
+        if (DesignerProperties.GetIsInDesignMode(this) == false)
+        {
+            width = double.IsNaN(Width) == false ? Width : availableSize.Width;
+            height = double.IsNaN(Height) == false ? Height : availableSize.Height;
+        }
+
+        TemplateSettings = new TemplateSettingValues(Math.Min(width, height));
+        return base.MeasureOverride(availableSize);
+    }
+
+    static void IsActiveChanged(DependencyObject d, DependencyPropertyChangedEventArgs args)
+    {
+        var tr = d as TrackRing;
+        if (tr == null || args.NewValue == null) { return; }
+        var isActive = (bool)args.NewValue;
+        tr.UpdateState(isActive);
+    }
+
+    void UpdateState(bool isActive)
+    {
+        if (_hasAppliedTemplate)
+        {
+            var state = isActive ? VisualStates.StateActive : VisualStates.StateInactive;
+            VisualStateManager.GoToState(this, state, true);
+        }
+    }
+}
+
+public class TemplateSettingValues : DependencyObject
+{
+    #region [Dependency Properties]
+    public static readonly DependencyProperty EllipseDiameterProperty = DependencyProperty.Register(
+        nameof(EllipseDiameter),
+        typeof(double),
+        typeof(TemplateSettingValues),
+        new PropertyMetadata(0D));
+    public double EllipseDiameter
+    {
+        get => (double)GetValue(EllipseDiameterProperty);
+        set => SetValue(EllipseDiameterProperty, value);
+    }
+
+    public static readonly DependencyProperty EllipseOffsetProperty = DependencyProperty.Register(
+        nameof(EllipseOffset),
+        typeof(Thickness),
+        typeof(TemplateSettingValues),
+        new PropertyMetadata(default(Thickness)));
+    public Thickness EllipseOffset
+    {
+        get => (Thickness)GetValue(EllipseOffsetProperty);
+        set => SetValue(EllipseOffsetProperty, value);
+    }
+
+    public static readonly DependencyProperty MaxSideLengthProperty = DependencyProperty.Register(
+        nameof(MaxSideLength),
+        typeof(double),
+        typeof(TemplateSettingValues),
+        new PropertyMetadata(0D));
+    public double MaxSideLength
+    {
+        get => (double)GetValue(MaxSideLengthProperty);
+        set => SetValue(MaxSideLengthProperty, value);
+    }
+    #endregion
+
+    public TemplateSettingValues(double width)
+    {
+        if (width <= 0.1)
+            width = 1.0;
+
+        if (width <= 40)
+            EllipseDiameter = width / 10 + 1;
+        else
+            EllipseDiameter = width / 10;
+
+        MaxSideLength = width - EllipseDiameter;
+        EllipseOffset = new Thickness(0, EllipseDiameter * 2.0, 0, 0);
+    }
+}
